edea94a0fb68d364f20bab3e2cf8939014015d39
VivekCodes05 <vk154523@gmail.com>
2025-03-27 03:04:48 +0000

    type_conversion

---
+++ .devcontainer/01_basics/type_conversion.js
@@ -0,0 +1,16 @@
+let num=33
+console.log(typeof num)
+let username="vivek"
+console.log(typeof username)
+let isLoggedIn=true
+console.log(isLoggedIn)
+
+let stringnum=String(num)
+console.log(typeof stringnum)
+console.log(stringnum)
+
+username=true
+let Numberusername=Number(username)         //"33" -> 33 & number
+console.log(typeof Numberusername)          //"33abc" -> NaN & number
+console.log(Numberusername)                //true   -> 1 & number
+
