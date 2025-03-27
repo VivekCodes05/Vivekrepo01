292ee6f654564bb0cf791f2a1bc3a8285bab8b6e
VivekCodes05 <vk154523@gmail.com>
2025-03-27 18:33:23 +0000

    data types done

---
+++ .devcontainer/01_basics/data_types.js
@@ -0,0 +1,27 @@
+//two types of data types
+//primitive ->7
+      //1. number,
+      //2. string,
+      //3. boolean,
+      //4. null,   let a=null
+                //console.log(typeof a)     => object
+      //5. undefined,    if a variable is declared but not assigned anything ,it is undefined
+                    //  also "undefined" can be assigned to it
+      //6. symbol,        used to make  variables and object differ and unique even if their value is same
+          //let Id=Symbol("123") 
+          //let Id2=Symbol("123")
+          //console.log(Id==Id2)    => false
+      //7. BigINT
+
+//non primitive or reference
+        //array,
+                //let num=[1,2,3,4]
+        // objects,     -> key value pair so everything inside {bracket} combindly called object
+                        // also it can be assigned to a variable
+                //{
+             // name="india"
+             // code=92}
+        // function      ->it can also be assigned to a variable
+                //function(){
+                //console.log("hello")}
+    
