2403d200134690710ce39c800bf49a5e04b85d2a
VivekCodes05 <vk154523@gmail.com>
2025-04-07 07:49:59 +0000

    math done

---
+++ .devcontainer/01_basics/math.js
@@ -0,0 +1,11 @@
+console.log((Math.random()*10)+1)   //random method of object MATH return random value 0(inclsive) and 1(exclusive)
+        // *10 return b/w 0 and 10
+        // +1 shift range from 1 to 10
+const max=30
+const min=10
+console.log((Math.random()*(max-min))+min)
+
+console.log(Math.max(1,2,3,4))
+console.log(Math.min(1,2,3,4))
+console.log(Math.pow(2,3))
+
