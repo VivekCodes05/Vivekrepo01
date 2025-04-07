62d439afcdb16a52ed2523bd823fb63963920526
VivekCodes05 <vk154523@gmail.com>
2025-04-07 06:18:30 +0000

    number done

---
+++ .devcontainer/01_basics/number.js
@@ -0,0 +1,13 @@
+let rep=400
+// console.log(rep)
+// console.log(typeof (rep))
+// let newstr=rep.toString()
+// console.log(newstr)
+// console.log(typeof newstr)
+rep=8987675.
+console.log(rep.toFixed(2))      //add specified no. of 0's with a decimal
+console.log(rep.toLocaleString())    //returns no. in USA format
+ console.log(rep.toLocaleString('en-IN'))   //returns no. in INDIAN format
+
+let newnum=671.864
+console.log(newnum.toPrecision(4)) //return precise no. till specified no. and rounds off last digit of the no.
