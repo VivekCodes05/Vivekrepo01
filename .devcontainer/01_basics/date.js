864c7f4eccf51c043941197c4a539ffe5178fc10
VivekCodes05 <vk154523@gmail.com>
2025-04-08 02:21:08 +0000

    date done

--- .devcontainer/01_basics/date.js
+++ .devcontainer/01_basics/date.js
@@ -9,8 +9,17 @@ console.log(date.toDateString())
 
 let newDate=new Date(2023,0,20)     //months start from 0
 console.log(newDate.toDateString())
-newDate="2023-12-15"
+newDate="2023-12-15"     //this is normal month numbering 1 to 12
 console.log(newDate)
 let mytimestamp=Date.now()
 console.log(mytimestamp)
-//console.log(newDate.getTime())
+console.log(date.getTime())    //gives the time in milisec from jan 1 1970 to now
+
+//convert milisec to sec
+let otherdate=new Date()
+ console.log(Math.floor(otherdate.getTime()/1000))   //floor is additional just to remove decimal values
+
+ //customized using parameters of method toLocaleString()
+ console.log(otherdate.toLocaleString('default',{
+    weekday : "long"
+ }))
