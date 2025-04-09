3fac59aeec8fdff105594a73758bcb4ae779f012
VivekCodes05 <vk154523@gmail.com>
2025-04-09 20:33:13 +0000

    json api intro done

--- .devcontainer/02_basics/json_api_intro.js
+++ .devcontainer/02_basics/json_api_intro.js
@@ -1,9 +1,9 @@
 //API - act as waiter b/w frontend and backend for exchange of data
-//          API's are there in JSON format earlier they were in XML file
-//          first json file format of an api(mostly)
+//          API's are there in JSON format earlier ,they were in XML file
+//          first format- json file format of an api(mostly)
 {
-    "name":"india",
-    "class":"sixth"    //there is no error in this but require something more
+ //   "name":"india",
+  //  "class":"sixth"    //there is no error in this but require something more
 }
 //         second format - array of objects
 [
